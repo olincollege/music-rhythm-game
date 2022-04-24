6318069f9726d1a9bd08b6404eb1df76006b9d28
Isabel de Luis <ideluis@olin.edu>
2022-04-23 23:05:34 -0400

    Added the arrow sprite and wrote arrow class

---
+++ scripts/arrow_class.py
@@ -0,0 +1,32 @@
+"""
+Class for the arrow sprite
+"""
+import pygame as pg
+
+class Arrow:
+    """
+    Arrow Sprite with basic rotation functionality
+
+    Attributes:
+        -_arrow_sprite: An image of the arrow sprite
+    """
+    
+    def __init__(self):
+        """
+        Create a new instance of the arrow sprite
+        """
+        _arrow_sprite = pg.image.load("scripts/Arrow Sprite.png")
+
+
+    def rotate(self,angle):
+        """
+        Rotates the arrow sprite
+
+        Arguments:
+            -Angle: an integer representing how much you want to rotate the 
+                    arrow
+        
+        Returns:
+            -None
+        """
+        self._arrow_sprite = pg.transform.rotate(self._arrow_sprite, angle)
