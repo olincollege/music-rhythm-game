307014ad8192ec81eef9d9ecc3201d6499721c9b
Madie Tong <madietng@gmail.com>
2022-05-08 13:47:44 -0400

    fixed arrow file

--- scripts/arrow.py
+++ scripts/arrow.py
@@ -16,20 +16,30 @@ class Arrow(pg.sprite.Sprite):
         """
         Create a new instance of the arrow sprite that is scaled and rotated.
         """
-        self._surface = pg.image.load("/home/mtong1/music-rhythm-game/assets/imgs/arrow_sprite.png").convert_alpha()
-        self._surface = pg.transform.smoothscale(self._surface, (100, 100))
+        self._surface = pg.image.load("assets/imgs/arrow_sprite.png").convert_alpha()
 
         if direction == "up":
+            self._surface = pg.image.load("assets/imgs/green_arrow.png").convert_alpha()
             self._surface = pg.transform.rotate(self._surface, 90)
         if direction == "down":
+            self._surface = pg.image.load("assets/imgs/blue_arrow.png").convert_alpha()
             self._surface = pg.transform.rotate(self._surface, 270)
         if direction == "right":
+            self._surface = pg.image.load("assets/imgs/yellow_arrow.png").convert_alpha()
             self._surface = pg.transform.rotate(self._surface, 0)
         if direction == "left":
+            self._surface = pg.image.load("assets/imgs/pink_arrow.png").convert_alpha()
             self._surface = pg.transform.rotate(self._surface, 180)
+
+        self._surface = pg.transform.smoothscale(self._surface, (95, 95))
+
+        
         self._direction = direction
         self._x = 0
         self._y = 0
+
+    def get_position(self):
+        return self._x, self._y
 
     def set_position(self, x, y):
         self._x = x
@@ -40,15 +50,15 @@ class Arrow(pg.sprite.Sprite):
 
 
 
-    def direction(self):
-        if self._direction == "up":
-            self._surface = pg.transform.rotate(self._surface, 90)
-        if self._direction == "down":
-            self._surface = pg.transform.rotate(self._surface, 270)
-        if self._direction == "right":
-            self._surface = pg.transform.rotate(self._surface, 0)
-        if self._direction == "left":
-            self._surface = pg.transform.rotate(self._surface, 180)
+    # def direction(self):
+    #     if self._direction == "up":
+    #         self._surface = pg.transform.rotate(self._surface, 90)
+    #     if self._direction == "down":
+    #         self._surface = pg.transform.rotate(self._surface, 270)
+    #     if self._direction == "right":
+    #         self._surface = pg.transform.rotate(self._surface, 0)
+    #     if self._direction == "left":
+    #         self._surface = pg.transform.rotate(self._surface, 180)
 
 
     def display_arrow(self, screen):
@@ -56,8 +66,6 @@ class Arrow(pg.sprite.Sprite):
         Displays the arrow on top of the screen.
         """
         screen.blit(self._surface, (self._x, self._y))
-
-
 
 
     def update(self):
@@ -68,7 +76,7 @@ class Arrow(pg.sprite.Sprite):
 
 
     def at_top_screen(self):
-        if self._y < 0:
+        if self._y < -60:
             return True
         return False
 
@@ -82,13 +90,13 @@ class PlayerArrow(Arrow):
 
     def position(self):
         if self._direction == "up":
-            self.set_position(500, 600)
+            self.set_position(913, 600)
         if self._direction == "left":
-            self.set_position(590, 600)
+            self.set_position(1000, 600)
         if self._direction == "right":
-            self.set_position(670, 600)
+            self.set_position(1150, 600)
         if self._direction == "down":
-            self.set_position(800, 600)
+            self.set_position(1077, 600)
 
     def type(self):
         return "player"
@@ -100,13 +108,13 @@ class ComputerArrow(Arrow):
 
     def position(self):
         if self._direction == "up":
-            self.set_position(10, 600)
+            self.set_position(15, 600)
         if self._direction == "left":
-            self.set_position(100, 600)
+            self.set_position(105, 600)
         if self._direction == "right":
-            self.set_position(180, 600)
+            self.set_position(257, 600)
         if self._direction == "down":
-            self.set_position(255, 600)
+            self.set_position(180, 600)
 
     def type(self):
         return "computer"
