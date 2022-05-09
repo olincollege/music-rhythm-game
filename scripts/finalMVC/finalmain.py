c0c6ef5937bcb6fe63d5147061d73e749cc071eb
richardli03 <rli@olin.edu>
2022-05-09 11:27:08 -0400

    testing the final MVC organization

---
+++ scripts/finalMVC/finalmain.py
@@ -0,0 +1,280 @@
+from finalcontroller import *
+from finalmodel import *
+from finalview import *
+
+pg.init()
+game_timer = 0
+showarrows = True
+WIDTH, HEIGHT = 1280, 720
+BPM = 100  # tempo of the song
+# DELAY = 3.59
+# DELAY = 3.74
+DELAY = 3.44 # Changes based on arrow speed.
+
+# screen size
+size = (WIDTH, HEIGHT)
+screen = pg.display.set_mode(size)
+
+# window title
+pg.display.set_caption("catJAM")
+
+# game clock (fps)
+clock = pg.time.Clock()
+
+dt = clock.tick(60)/1000
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
+clock = pygame.time.Clock()
+
+# All the scenes.
+scenes = {}
+scenes["menu"] = Menu(screen, scenes)
+scenes["game"] = Game(screen, scenes)
+scenes["exit"] = Exit(screen, scenes)
+# Start with the menu.
+scene = scenes["menu"]
+scene.start()
+
+# Produce arrows only when arrows are needed
+
+# initialized stationary arrows at top of screen
+comp_up = Arrow("up")
+comp_up.set_position(10,20)
+comp_left = Arrow("left")
+comp_left.set_position(100, 20)
+comp_down = Arrow("down")
+comp_down.set_position(180, 20)
+comp_right = Arrow("right")
+comp_right.set_position(255,10)
+
+
+p_up = Arrow("up")
+p_up.set_position(910,20)
+p_left = Arrow("left")
+p_left.set_position(1000, 20)
+p_down = Arrow("down")
+p_down.set_position(1080, 20)
+p_right = Arrow("right")
+p_right.set_position(1155,10)
+# # Background Cats
+
+dj_cat = Cat("dj", 460, 350)
+speaker_cat = Cat("speaker", 500, 65)
+
+# Team 1 Cats
+piano_cat = Cat("piano", 1000, 300)
+guitar_cat = Cat("guitar", 700, 350)
+drum_cat = Cat("drum", 850, 225)
+
+# Team 2 Cats
+piano_cat_2 = Cat("piano", -50, 300, True)
+guitar_cat_2 = Cat("guitar", 205, 350, True)
+drum_cat_2 = Cat("drum", 77, 225, True)
+# lists to keep track of arrows on screen
+
+up_arrows = []
+left_arrows = []
+right_arrows = []
+down_arrows = []
+
+song_information = [1, 1, 1, 1, 4,
+                    1, 1, 1, 1, 3.75,
+                    1, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1, 0.5, 0.5, 0.5, 0.5, 0.5, 0.25,
+                    1, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1, 0.5, 0.5, 0.5, 0.5, 0.5, 0.25,    
+                    0.5, 0.5, 0.5, 0.5, 1, 2, 0.5, 0.5, 1, 0.75,
+                    0.5, 0.5, 0.5, 0.5, 1, 2, 0.5, 0.5, 1, 0.75,
+                    0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25,
+                    0.25, 0.25, 0.5, 0.5, 1, 1, 1, 0.85,
+                    0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25,
+                    0.25, 0.25, 0.5, 0.5, 1, 1, 1, 0.85,
+                                    ]
+num_notes = [5, 5, 13, 12,9,9,17 ,17]
+# test_val = 1
+# song_information = [test_val,test_val,test_val,test_val,test_val,test_val,test_val,test_val,test_val,test_val,test_val,test_val,test_val,test_val,test_val,test_val,test_val,test_val,test_val]
+arrows_on_screen = []
+computer_arrows = []
+player_arrows = [[], [], [], []]
+total_score = 0
+
+
+add_arrow = pg.USEREVENT + 0
+add_arrow_event = pg.event.Event(add_arrow)
+
+
+deletion = 0
+note_counter = 0
+melody_counter = 0
+saved_time = 0
+next_notes = melody_arrow_generator(BPM, song_information)
+# print(next_notes)
+next_note = next_notes[0]
+melody = 0
+while True:
+
+    # if game_timer % length == True:
+    #     pg.time.set_timer(add_arrow, length)
+    game_timer = game_timer + dt
+    print (f"game timer {game_timer}")
+
+    if scene == scenes["game"]:
+        if deletion == 1:
+            # print(f"game timer before{game_timer}")
+            game_timer -= 0.09
+            # print(f"game timer after{game_timer}")
+            deletion = 0
+
+        # The intro before the game begins = DELAY
+        if game_timer > DELAY:
+            if game_timer > next_note + saved_time and note_counter < len(next_notes):
+                pg.event.post(add_arrow_event)
+                # print(f"post time added {game_timer - saved_time}")
+                next_note = next_notes[note_counter]
+                saved_time = game_timer
+                # print(f"counter {counter}")
+                note_counter += 1
+                melody_counter += 1
+
+    events = pg.event.get()
+    # print(type(events))
+    # print(game_timer)
+    # Switch scenes if there is a new scene.
+    new_scene = scene.update(events, dt)
+    # print (i)
+
+    if new_scene is not scene:
+        # print("new scene")
+        # If there is a new scene, make sure to allow the old
+        # scene to exit and the new scene to start.
+        scene.exit()
+        scene = new_scene
+        game_timer = 0
+        scene.start()
+
+    # pygame.display.update()
+
+    for event in events:
+        if event.type == pg.QUIT:
+            pg.quit()
+            exit()
+
+        if event.type == add_arrow and game_timer > DELAY:
+            # print(f"melody: {melody}")
+            # print(f"melody_counter: {melody_counter}")
+            
+            # if the number of notes in this melody is greater than the number of notes for this melody
+            if melody_counter > num_notes[melody]:
+                # go onto the next melody
+                melody += 1
+                # reset the counter
+                melody_counter = 0
+                
+            # If the melody number is divisible by 2, that means it's the computer's turn
+            if melody % 2 == 0:
+                print("computer")
+                computer_produce_arrow(computer_arrows,arrows_on_screen)
+            else:
+                print("player")
+                try:
+                    player_produce_arrow(player_arrows,arrows_on_screen)
+                    arrows_on_screen.pop(0)
+                except IndexError:
+                    print("back to the computer")
+                
+                
+
+        #keybinds
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_UP:
+            #if the number of up arrows in the player's side is not zero, 
+            # then you can delete the first arrow when key is pressed
+                if len(player_arrows[0]) != 0:
+                    total_score += score_calc(player_arrows[0][0]._y, "up")
+                try:
+                    del player_arrows[0][0]
+                except:
+                    pass
+
+            elif event.key == pg.K_LEFT:
+                if len(player_arrows[1]) != 0:
+                    total_score += score_calc(player_arrows[1][0]._y, "left")
+                try:
+                    del player_arrows[1][0]
+                except:
+                    pass
+            
+            elif event.key == pg.K_RIGHT:
+                if len(player_arrows[2]) != 0:
+                    total_score += score_calc(player_arrows[2][0]._y, "right")
+                try:
+                    del player_arrows[2][0]
+                except:
+                    pass
+            
+            elif event.key == pg.K_DOWN:
+                if len(player_arrows[3]) != 0:
+                    total_score += score_calc(player_arrows[3][0]._y, "down")
+                try:
+                    del player_arrows[3][0]
+                except: 
+                    pass
+            
+
+    # display the arrows at the top of the screen if not in menu
+    if scene == scenes["game"]:
+        # new_computer_arrows = [[], [], [], []]
+        # new_player_arrows = [[], [], [], []]
+          # display moving arrows if not in menu scene
+        for arrow in computer_arrows:
+            arrow.display_arrow(screen)
+            arrow.update()
+
+        for class_type in player_arrows:
+            for arrow in class_type:
+                arrow.display_arrow(screen)
+                arrow.update()
+
+        # stationary arrows
+        comp_left.display_arrow(screen)
+        comp_down.display_arrow(screen)
+        comp_up.display_arrow(screen)
+        comp_right.display_arrow(screen)
+        
+        p_left.display_arrow(screen)
+        p_down.display_arrow(screen)
+        p_up.display_arrow(screen)
+        p_right.display_arrow(screen)
+        # dj_cat.display(screen)
+        
+        # drum_cat.display(screen)
+        # piano_cat.display(screen)
+        # speaker_cat.display(screen)
+        # guitar_cat.display(screen)
+
+        # drum_cat_2.display(screen)
+        # piano_cat_2.display(screen)
+        # guitar_cat_2.display(screen)
+    
+#potential off screen delete solution
+    for arrow in computer_arrows + player_arrows[0] + player_arrows[1] + \
+        player_arrows[2] + player_arrows[3]:
+        if arrow.at_top_screen():
+            if arrow.class_type() == "computer":
+                computer_arrows.remove(arrow)
+            
+            elif arrow.class_type() == "player":
+                if arrow.get_direction() == "up":
+                    #player_arrows[0].remove(arrow)
+                    player_arrows[0].remove(arrow)
+
+                elif arrow.get_direction() == "left":
+                    player_arrows[1].remove(arrow)
+
+                elif arrow.get_direction() == "right":
+                    player_arrows[2].remove(arrow)
+
+                elif arrow.get_direction() == "down":
+                    player_arrows[3].remove(arrow)
+    # updates whats on screen everytime loop runs through
+    pg.display.update()
+    # 60 fps
+    clock.tick(60)/1000
---
+++ scripts/finalMVC/finalmodel.py
@@ -0,0 +1,371 @@
+"""
+Model of catJAM game
+Note from Isa:
+Some more unused arguments--richard take a look and see if we actually need
+those
+"""
+# Disabled pylint warnings that would break our game if fixed
+# pylint: disable=wildcard-import
+# pylint: disable=no-member
+# pylint: disable=unused-wildcard-import
+# pylint: disable=invalid-name
+
+import pygame
+from finalview import *
+from finalcontroller import *
+pygame.init()
+
+class Game():
+    """
+    A Game interface scene for catJAM
+
+    Attributes:
+        scenes: the dictionary containing all of the scenes to be cycled through
+        screen: the dimensions of the screen the scene will be placed on
+        music: the music associated with the menu screen
+        channel: the channel that said music will be played on
+        background: an instance of the Background class with the background
+                    image
+
+    """
+
+    def __init__(self, screen, scenes):
+        """
+        Creates an instance of the Game class.
+        """
+        self.scenes = scenes
+        self.screen = screen
+        self.font = pygame.font.SysFont('freesansbold.ttf', 32)
+        self.music = pygame.mixer.Sound(
+            "../assets/soundtrack/melodies with intro.wav")
+        self.channel = pygame.mixer.Channel(0)
+        self.game_timer = 0
+        self.background = Background('../assets/imgs/Background.png', [0, 0])
+
+    def start(self):
+        """
+        Starts the scene by laying down the background image and playing the
+        music
+        """
+        self.channel.play(self.music, loops=0, fade_ms=0)
+        self.screen.blit(self.background.image, self.background.rect)
+        #print("playing" + str(game_timer))
+
+
+    def update(self, events, dt):
+        """
+        Updates the screen based on what events have occured. If the soundtrack
+        has finished playing, return to the menu class.
+
+        Args:
+            events (list): a list of all of the events that have occured in one
+                           cycle of the game loop
+            dt (int): the amount of time that has passed since the last cycle of
+                      the game loop
+
+        Returns:
+            self.scenes['menu']: the Menu class, the next scene, if 42.3 seconds
+                                 pass.
+            self: essentially, nothing, if 42.3 seconds has not passed.
+        """
+        self.game_timer += dt
+        if self.game_timer >= 42.3:
+            return self.scenes['exit']
+        self.screen.blit(self.background.image, self.background.rect)
+
+        return self
+
+    def exit(self):
+        """
+        Stops the music upon exit to avoid layering of soundtracks.
+        """
+        self.channel.stop()
+
+class Arrow(pg.sprite.Sprite):
+    """
+    A parent class that initializes the size, direction, and color of all
+    moving arrows. Also allows for easy control of arrow movement.
+
+    Attributes:
+        _surface:
+            An image of the arrow sprite. Different colors of the same arrow
+            are loaded for different instances dependent on which direction
+            it points in.
+        _direction:
+            A string representing the direction that the user wants the arrow
+            to point in. Direction determines which arrow image gets loaded,
+            how much the image is rotated, and where on the screen it initially
+            gets placed.
+        _x:
+            An integer representing the x-position of the sprite on the screen.
+            This gets used when displaying the sprite.
+        _y:
+            An integer representing the y-position of the sprite on the screen.
+            This gets used when displaying the sprite.
+    """
+
+    def __init__(self, direction):
+        """
+        Create a new instance of the arrow sprite that is scaled and rotated.
+        """
+        self._surface = pg.image.load("../assets/imgs/arrow_sprite.png").\
+            convert_alpha()
+
+        if direction == "up":
+            self._surface = pg.image.load("../assets/imgs/green_arrow.png")\
+                .convert_alpha()
+            self._surface = pg.transform.rotate(self._surface, 90)
+        if direction == "down":
+            self._surface = pg.image.load("../assets/imgs/blue_arrow.png").\
+                convert_alpha()
+            self._surface = pg.transform.rotate(self._surface, 270)
+        if direction == "right":
+            self._surface = pg.image.load("../assets/imgs/yellow_arrow.png").\
+                convert_alpha()
+            self._surface = pg.transform.rotate(self._surface, 0)
+        if direction == "left":
+            self._surface = pg.image.load("../assets/imgs/pink_arrow.png").\
+                convert_alpha()
+            self._surface = pg.transform.rotate(self._surface, 180)
+
+        self._surface = pg.transform.smoothscale(self._surface, (95, 95))
+        self._direction = direction
+        self._x = 0
+        self._y = 0
+
+    def get_position(self):
+        """
+        Returns the x and y position of image.
+        """
+        return self._x, self._y
+
+    def set_position(self, x, y):
+        """
+        Changes the x and y values.
+
+        Args:
+            x:
+                An integer representing the pixel position on the screen that
+                the user wants to set the sprite to.
+            y:
+                An integer representing the pixel position on the screen that
+                the user wants to set the sprite to.
+
+        """
+        self._x = x
+        self._y = y
+
+    def get_direction(self):
+        """
+        Returns the value of the private attribute _direction.
+        """
+        return self._direction
+
+    def display_arrow(self, screen):
+        """
+        Displays the arrow on top of the screen.
+        Args:
+            screen:
+                The background surface initialized using pygame on which the
+                game is displayed.
+        """
+        screen.blit(self._surface, (self._x, self._y))
+
+    def update(self):
+        """
+        Changes the arrow's y position. When run in the game loop, the arrow
+        will move up the screen by 2 pixels.
+        """
+        self._y -= 10
+
+    def at_top_screen(self):
+        """
+        Returns True when the arrow is partially off-screen, or when the
+        arrow's y-position is less than -60. If the arrow is still
+        properly on-screen, returns False.
+        """
+        if self._y < -60:
+            return True
+        return False
+
+    def __repr__(self) -> (str):
+        """
+        Returns a human-readable format of the direction.
+        """
+        return f"{self._direction}"
+
+class PlayerArrow(Arrow):
+    """
+    A subclass of the Arrow class that initializes arrows on the "player"
+    side of the screen.
+    """
+    def __init__(self, direction):
+        """
+        Constructs all necessary attributes for positioning the
+        computer arrows.
+        """
+        super().__init__(direction)
+
+    def position(self):
+        """
+        Uses the parent class function set_position to initialize the starting
+        position of the player arrows on the screen depending on which
+        direction arrow it is.
+        """
+        if self._direction == "up":
+            self.set_position(913, 600)
+        if self._direction == "left":
+            self.set_position(1000, 600)
+        if self._direction == "right":
+            self.set_position(1150, 600)
+        if self._direction == "down":
+            self.set_position(1077, 600)
+
+    def class_type(self):
+        """
+        Returns a string that indicates to a user which class arrow it is.
+        """
+        return "player"
+
+class ComputerArrow(Arrow):
+    """
+    A subclass of the Arrow class that initializes arrows on the "computer"
+    side of the screen.
+    """
+    def __init__(self, direction):
+        """
+        Constructs all necessary attributes for positioning the
+        computer arrows.
+        """
+        super().__init__(direction)
+
+    def position(self):
+        """
+        Uses the parent class function set_position to initialize the starting
+        position of the computer arrows on the screen depending on which
+        direction arrow it is.
+        """
+        if self._direction == "up":
+            self.set_position(15, 600)
+        if self._direction == "left":
+            self.set_position(105, 600)
+        if self._direction == "right":
+            self.set_position(257, 600)
+        if self._direction == "down":
+            self.set_position(180, 600)
+
+    def class_type(self):
+        """
+        Returns a string that indicates to a user which class arrow it is.
+        """
+        return "computer"        
+        
+def melody_arrow_generator(BPM, song_info):
+    """
+    Each int in the song_info list corresponds to how many beats that note will
+    last.
+    For example, a 1 means it will last 1 beat (a quarter note). A 4 means it
+    will last 4 beats (a whole note)
+
+    Args:
+        BPM: an int containing the beats per minute of a tempo
+        song_info: a list containing the length of the melody notes for a song. 
+
+    Returns: A list containing the arrow information for a melody
+    """
+    next_notes = []
+    
+    multiplier = 1.35
+    for note in song_info:
+        quarter_note_length = 60*1000/BPM
+        # error correction for game lag
+        if note == 4:
+            quarter_note_length = quarter_note_length - 48*multiplier
+        if note == 2:
+            quarter_note_length = quarter_note_length - 24*multiplier
+        if note == 1:
+            quarter_note_length = quarter_note_length - 13*multiplier
+        if note == 0.5:
+            quarter_note_length = quarter_note_length - 40*multiplier
+        if note == 0.25:
+            quarter_note_length = quarter_note_length - 70*multiplier
+
+        next_notes.append(quarter_note_length*note/1000)
+    return next_notes
+
+def computer_produce_arrow(computer_arrows, arrows_on_screen):
+    """
+    A function that produces a random arrow when called and adds the
+    directional arrow to a corresponding list. This function is specifically
+    used to produce arrows on the computer's side.
+
+    Arguments:
+        -computer_arrows: a list of arrows the computer produces
+        -arrows_on screen: a list of arrows currently on the screen
+    Returns:
+        -computer_arrows: a list of arrows the computer produces
+        -arrows_on screen: a list of arrows currently on the screen
+    """
+    arrows = ["up", "left", "right", "down"]
+
+    random_arrow = random.choice(arrows)
+    new_arrow = ComputerArrow(random_arrow)
+    new_arrow.position()
+    # print(new_arrow.get_position())
+
+    computer_arrows.append(new_arrow)
+
+    arrows_on_screen.append(random_arrow)
+
+    return computer_arrows, arrows_on_screen
+
+def score_calc(y_position, arrow_type):
+    """
+    Function to determine the score a player receives after they stop the arrow
+    from moving
+
+    Arguments:
+        -y_position: an integer value representing the y-position of the arrow
+                     when the player stopped it.
+        -arrow_type: a string representing the type of arrow (left, right, down,
+                     up)
+    
+    Returns:
+        -score: the score gained (will need to be added on total score in main 
+                function)
+    """
+
+    
+    if arrow_type == "right":
+        ARROW_POSITION = 10
+    else:
+        ARROW_POSITION = 20
+
+    diff = abs(ARROW_POSITION - y_position)
+    score = 0
+
+    if diff == 0:
+        score += 1
+    elif diff > 0 and diff <= 10:
+        score += .9
+    elif diff > 10 and diff <= 20:
+        score += .8
+    elif diff > 20 and diff <= 30:
+        score += .7
+    elif diff > 30 and diff <= 40:
+        score += .6
+    elif diff > 40 and diff <= 50:
+        score += .5
+    elif diff > 50 and diff <= 60:
+        score += .4
+    elif diff > 60 and diff <= 70:
+        score += .3
+    elif diff > 70 and diff <= 80:
+        score += .2
+    elif diff > 80 and diff <= 90:
+        score += .1
+    elif diff > 90:
+        score += 0
+
+    return score
+
